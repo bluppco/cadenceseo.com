d104e4d1b38a8b66cb47669ec8e43c75676424ba
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-03-08 16:59:59 +0530

    feat: optimized home page for mobile

--- src/components/home/newsletter/form/jsx/index.jsx
+++ src/components/home/newsletter/form/jsx/index.jsx
@@ -1,9 +1,9 @@
 const Form = ( props ) => {
 
     return(
-        <div className="flex items-center pt-16">
-            <input className="h-12 w-[600px] bg-transparent border border-cadence_gray"></input>
-            <button className="bg-white h-12 px-14 flex items-center uppercase font-roboto font-medium tracking-wide">Subscribe</button>
+        <div className="flex flex-col md:flex-row gap-2 md:gap-2 items-center pt-8 md:pt-16 w-full">
+            <input className="h-12 w-full md:w-[600px] bg-transparent border border-cadence_gray"></input>
+            <button className="bg-white w-full h-12 px-14 flex items-center justify-center uppercase font-roboto font-medium tracking-wide">Subscribe</button>
         </div>
     )
 
