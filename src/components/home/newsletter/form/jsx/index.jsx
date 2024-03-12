ae18158004eb0e9b9191b0e81e65017e99e82f65
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-03-12 10:42:09 +0530

    fix: fixed width of newsletter component

--- src/components/home/newsletter/form/jsx/index.jsx
+++ src/components/home/newsletter/form/jsx/index.jsx
@@ -1,8 +1,8 @@
 const Form = ( props ) => {
 
     return(
-        <div className="flex flex-col md:flex-row gap-2 md:gap-2 items-center pt-6 md:pt-16 w-full">
-            <input className="h-12 w-full md:w-[600px] bg-transparent border border-cadence_gray"></input>
+        <div className="flex flex-col md:flex-row gap-2 md:gap-0 items-center pt-6 md:pt-16 w-full">
+            <input className="h-12 w-full md:min-w-[600px] bg-transparent border border-cadence_gray"></input>
             <button className="bg-white w-full h-12 px-14 flex items-center justify-center uppercase font-roboto font-medium tracking-wide">Subscribe</button>
         </div>
     )
